9fda82496d2aad5000d739e5c4020ba1bdf61273
bishar08 <abubushayra@gmail.com>
2022-03-14 17:21:36 -0500

    first comment api

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Featured from './components/Featured'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Featured />
+    </>
+  )
+}
+
+export default App
